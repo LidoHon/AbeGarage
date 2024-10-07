8df01fbed1a56ca01f8a4e6bff400d020d1e10fb
Melat t <melattesfaye16@gmail.com>
2024-10-07 20:27:28 +0300

    orders route middleware

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const ordersController = require("../controllers/order.controller");
+const middleware = require("../middlewares/auth.middleware")
 
 //This created a new order
-router.post("/api/order", ordersController.createOrder);
+router.post("/api/order",middleware.verifyToken ,ordersController.createOrder);
 
-router.get("/api/orders", ordersController.getAllOrders);
+router.get("/api/orders", middleware.verifyToken,ordersController.getAllOrders);
 
-router.get("/api/order/:id", ordersController.getOrderById);
+router.get("/api/order/:id", middleware.verifyToken,ordersController.getOrderById);
 
-router.put("/api/order", ordersController.updateOrder);
+router.put("/api/order", middleware.verifyToken,ordersController.updateOrder);
 
-router.delete("/order/:id", ordersController.deleteOrder);
+router.delete("/order/:id",middleware.verifyToken, ordersController.deleteOrder);
 
 module.exports = router;
